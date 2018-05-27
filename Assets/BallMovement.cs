95f10b27b35fd060fe18bbec93735f2e3d6b6e85
Bradley Wascher <BradleyWascher@gmail.com>
2018-05-26 21:14:14 -0400

    Updated test scene, added in UI for the stroke count

--- Assets/BallMovement.cs
+++ Assets/BallMovement.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BallMovement : MonoBehaviour
 {
     float startingScreenPos;
     float endingScreenPos;
     public float length;
-    public float powerSensitivity = 0.1f;
+    public float powerSensitivity = 0.05f;
     public Rigidbody ball;
+    public Slider powerSlider;
+    private int strokeTotal = 0;
+    public Text strokeText;
     // Use this for initialization
     void Start()
     {
@@ -18,27 +22,38 @@ public class BallMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //print(Input.GetAxis("Mouse Y"));
+
+        if (powerSlider != null)
+        {
+            powerSlider.value = length; // set the power slider to the value of the power of the shot
+
+        }
+
 
         if (Input.GetMouseButtonDown(0) == true)
         {
             startingScreenPos = Input.GetAxis("Mouse Y");
-            
+
         }
 
         if (Input.GetMouseButton(0) == true)
         {
             endingScreenPos -= Input.GetAxis("Mouse Y") * powerSensitivity;
-            length = startingScreenPos - endingScreenPos;
+            length = Mathf.Clamp(startingScreenPos - endingScreenPos, 0f, 1f);
         }
 
         if (Input.GetMouseButtonUp(0) == true)
         {
-           
+
             ball.AddForce(new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z) * length, ForceMode.Impulse);
             //print(ball.gameObject.transform.forward * lenght);
             endingScreenPos = 0f;
             length = 0f;
+            strokeTotal++;
+            if (strokeText != null)
+            {
+                strokeText.text = strokeTotal.ToString();
+            }
         }
     }
 }
